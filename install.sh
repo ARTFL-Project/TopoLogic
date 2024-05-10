a076601488e39476bd1767860eaa775b26d70cec
Clovis Gladstone <clovisgladstone@gmail.com>
2024-05-10 10:11:24 -0500

    move to virtual env for global install

--- install.sh
+++ install.sh
@@ -1,11 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-sudo pip3 install lib/. --upgrade
+# Install virtualenv to sidestep venv (python3-venv installs python3-setuptools which causes issues on Ubuntu 22.04)
+pip3 install virtualenv
 
-
-echo "\nMoving web application components into place..."
+# Give current user permission to write to /var/lib/topologic
 sudo mkdir -p /var/lib/topologic
+sudo chown -R $USER:$USER /var/lib/topologic
 
+# Create the virtual environment
+virtualenv /var/lib/topologic/topologic_env
+source /var/lib/topologic/topologic_env/bin/activate
+pip3 install lib/. --upgrade
+deactivate
+
+# Install the topologic script
+sudo cp topologic /usr/local/bin/
+sudo chmod +x /usr/local/bin/topologic
+
+echo -e "\nMoving web application components into place..."
 
 if [ -d /web-app/browser-app/node_modules ]
     then
@@ -33,7 +45,7 @@ sudo rm -rf /var/lib/topologic/web-app
 sudo cp -Rf web-app /var/lib/topologic
 sudo rm -rf /var/lib/topologic/config
 sudo cp -Rf config /var/lib/topologic
-echo "\n## IMPORTANT ##\nTopoLogic runs behind the Gunicorn web server. Make sure you configure the server start script at /var/lib/topologic/web-app/api/web_app_start.sh. You should also make sure it autostarts on boot.\n"
+echo -e "\n## IMPORTANT ##\nTopoLogic runs behind the Gunicorn web server. Make sure you configure the server start script at /var/lib/topologic/web-app/api/web_app_start.sh. You should also make sure it autostarts on boot.\n"
 
 
 
